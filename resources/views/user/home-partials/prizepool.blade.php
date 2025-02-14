c04530b837921680e2319194fb2eafc1cef9b159
shellyOei <oeishelly15@gmail.com>
2025-02-14 17:38:10 +0700

    prize pool done, animation left

--- resources/views/user/home-partials/prizepool.blade.php
+++ resources/views/user/home-partials/prizepool.blade.php
@@ -1,3 +1,17 @@
-<section id="prizepool" class="h-screen w-screen bg-[#dad7cf] flex items-center justiy-center ">
-    
+<section id="prizepool" class="h-screen w-screen bg-[#dad7cf] flex flex-col items-center justify-center ">
+    <h1 class="font-oxanium text-4xl md:text-5xl lg:text-7xl text-[#14240a] text-center font-extrabold mb-12">Prizepool</h1>
+    <div class="flex items-center justify-center w-[95%] sm:w-full mb-5 sm:mb-16 space-x-5 sm:space-x-16">
+        <div class="flex flex-col text-center space-y-2">
+            <h1 class="font-oxanium text-[#14240a] text-2xl sm:text-3xl lg:text-5xl">1st Place</h1>
+            <h1 class="font-quicksand font-bold p-3 sm:p-6 bg-[#14240a] text-[#dad7cf] rounded-3xl text-2xl sm:text-3xl lg:text-5xl">IDR 3.0 Mio ++</h1>
+        </div>
+        <div class="flex flex-col text-center space-y-2">
+            <h1 class="font-oxanium text-[#14240a] text-2xl sm:text-3xl lg:text-5xl">2nd Place</h1>
+            <h1 class="font-quicksand font-bold p-3 sm:p-6 bg-[#14240a] text-[#dad7cf] rounded-3xl text-2xl sm:text-3xl lg:text-5xl">IDR 2.0 Mio ++</h1>
+        </div>
+    </div>
+    <div class="flex flex-col items-center justify-center w-full space-y-2">
+        <h1 class="font-oxanium text-[#14240a] text-2xl sm:text-3xl lg:text-5xl">3rd Place</h1>
+            <h1 class="font-quicksand font-bold p-3 sm:p-6 bg-[#14240a] text-[#dad7cf] rounded-3xl text-2xl sm:text-3xl lg:text-5xl">IDR 1.0 Mio ++</h1>
+    </div>
 </section>
