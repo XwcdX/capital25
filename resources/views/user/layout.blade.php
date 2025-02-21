9be893e20f81510c11463b1114bce43fc2453c88
XwcdX <darkpuppet90@gmail.com>
2025-02-21 16:54:35 +0700

    chore: adding favicon

--- resources/views/user/layout.blade.php
+++ resources/views/user/layout.blade.php
@@ -4,6 +4,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+    <link rel="icon" type="image/png" sizes="512x512" href="{{ asset('assets/favicon-512.png') }}">
+    <link rel="icon" type="image/png" sizes="192x192" href="{{ asset('assets/favicon-192.png') }}">
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('assets/apple-touch-icon.png') }}">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>User | {{ $title }}</title>
     {{-- tailwindcss --}}
